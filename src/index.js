b2e1365b3fb71a9776f868ab072cbccf3c325436
OlenaKruts <krutslena@ukr.net>
2023-06-02 18:50:14 +0300

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,139 +1,101 @@
-import { loadMore } from './js/load_more_images.js';
-import { onSubmit } from './js/onSubmit.js';
+import GalleryService from './js/get_category.js';
+import { renderGallery } from './js/render_gallery';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#search-form');
 
-export const input = form.firstElementChild;
+const input = document.querySelector('input[name="searchQuery"]');
 const searchBtn = input.nextElementSibling;
 
-export const fetchGalerryBtn = document.querySelector('.load-more');
-
-const message = `<span class="message-end is-hidden">
+const fetchGalerryBtn = document.querySelector('.load-more');
+const message = `<span class="message-end">
           We're sorry, but you've reached the end of search results.
         </span>`;
 fetchGalerryBtn.insertAdjacentHTML('afterend', message);
 fetchGalerryBtn.style.display = 'none';
 
 export const messageEnd = document.querySelector('.message-end');
-//const messageEnd = document.querySelector('.message-end');
-//console.log(messageEnd);
+messageEnd.style.display = 'none';
 
 export const galleryWrapper = document.querySelector('.gallery');
 
-export const perPage = 40;
-
-export let data = input.value.trim();
-
-//const galleryWrapper = document.querySelector('.gallery');
 form.addEventListener('submit', onSubmit);
-
 fetchGalerryBtn.addEventListener('click', loadMore);
-// function loadMore() {
-//   let data = input.value.trim();
-//   page += 1;
-//   getCategory(data, page, perPage).then(data => {
-//     const totalPages = Math.floor(data.totalHits / perPage) + 1;
-//     if (page < totalPages) {
-//       renderGallery(data);
-//     } else if (page > totalPages) {
-//       //renderGallery(data);
-//       fetchGalerryBtn.style.display = 'none';
-//       messageEnd.classList.remove('is-hidden');
-//     } else {
-//       renderGallery(data);
-//       fetchGalerryBtn.style.display = 'none';
-//       messageEnd.classList.remove('is-hidden');
-//     }
-//   });
-// }
 
-// function onSubmit(event) {
-//   event.preventDefault();
-//   galleryWrapper.innerHTML = '';
+//=============================================//
+const imgGalleryService = new GalleryService();
+//==============================================//
+async function onSubmit(event) {
+  try {
+    event.preventDefault();
+    galleryWrapper.innerHTML = '';
+    const searchQuery = input.value.trim();
 
-//   fetchGalerryBtn.style.display = 'none';
-//   const page = 1;
+    if (!searchQuery) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      fetchGalerryBtn.style.display = 'none';
+      messageEnd.style.display = 'none';
 
-//   data = input.value.trim();
+      return;
+    }
 
-//   getCategory(data, page, perPage)
-//     .then(data => {
-//       if (data.hits.length > 0) {
-//         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-//         renderGallery(data);
-//         fetchGalerryBtn.style.display = 'block';
-//       } else {
-//         Notiflix.Notify.failure(
-//           'Sorry, there are no images matching your search query. Please try again.'
-//         );
-//         galleryWrapper.innerHTML = '';
-//       }
-//     })
-//     .catch(err => console.log('Error', err));
-// }
+    imgGalleryService.name = searchQuery;
+    imgGalleryService.page = 1;
+    imgGalleryService.hits = 0;
+    //event.currentTarget.reset();
 
-// async function getCategory(data, page, perPage) {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const API_KEY = '36746040-cd18ae9043608bd5e4c67aa1b';
-//   const params = new URLSearchParams({
-//     key: API_KEY,
-//     q: data,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true',
-//   });
+    const data = await imgGalleryService.getCategory();
+    if (data.hits.length == 0) {
+      fetchGalerryBtn.style.display = 'none';
+      messageEnd.style.display = 'none';
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
 
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}?${params}&page=${page}&per_page=${perPage}`
-//     );
+      return;
+    }
+    renderGallery(data);
 
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+    fetchGalerryBtn.style.display = 'block';
+    messageEnd.style.display = 'none';
+  } catch (error) {
+    console.log(error);
+  }
+}
+//========================================//
+async function loadMore() {
+  try {
+    const data = await imgGalleryService.getCategory();
+
+    if (data.hits.length == 0) {
+      console.log('data.hits.length', data.hits.length);
+      fetchGalerryBtn.style.display = 'none';
+      messageEnd.style.display = 'block';
+      return;
+    }
+    renderGallery(data);
+
+    imgGalleryService.hits += 40;
+
+    if (imgGalleryService.hits + 40 > imgGalleryService.totalHits) {
+      fetchGalerryBtn.style.display = 'none';
+      messageEnd.style.display = 'block';
+      return;
+    }
+    imgGalleryService.hits += 40;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//========================================//
 
 // добавляю классы для стилей//
 input.classList.add('field-search');
 searchBtn.classList.add('button-search');
-
-// function renderGallery(array) {
-//   messageEnd.classList.add('is-hidden');
-//   const markUp = array.hits
-//     .map(
-//       ({
-//         tags,
-//         likes,
-//         largeImageURL,
-//         webformatURL,
-//         comments,
-//         downloads,
-//         views,
-//       }) => {
-//         return `<div class="photo-card">
-//           <a class="gallery-item" href="${largeImageURL}">
-//       <img class="gallery-photo" src="${webformatURL}" alt="${tags}" width="320" loading="lazy" />
-//       </a>
-//       <div class="info">
-//         <p class="info-item">
-//           <b>Likes: ${likes}</b>
-//         </p>
-//         <p class="info-item">
-//           <b>Views: ${views}</b>
-//         </p>
-//         <p class="info-item">
-//           <b>Comments: ${comments}</b>
-//         </p>
-//         <p class="info-item">
-//           <b>Downloads: ${downloads}</b>
-//         </p>
-//       </div>
-//       </div>`;
-//       }
-//     )
-//     .join('');
-
-//   galleryWrapper.insertAdjacentHTML('beforeend', markUp);
-//   lightBox.refresh();
-// }
--- src/js/get_category.js
+++ src/js/get_category.js
@@ -1,23 +1,44 @@
 //import { page, perPage, data } from '../index.js';
 import axios from 'axios';
-export async function getCategory(name, page, perPage) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = '36746040-cd18ae9043608bd5e4c67aa1b';
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: name,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
 
-  try {
-    const response = await axios.get(
-      `${BASE_URL}?${params}&page=${page}&per_page=${perPage}`
-    );
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '36746040-cd18ae9043608bd5e4c67aa1b';
 
-    return response.data;
-  } catch (error) {
-    console.error(error);
+export default class GalleryService {
+  constructor() {
+    this.name = '';
+    this.page = 1;
+    this.hits = 0;
+    this.totalHits = 0;
+    this.perPage = 40;
   }
+
+  async getCategory() {
+    try {
+      const params = new URLSearchParams({
+        key: API_KEY,
+        q: this.name,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: this.page,
+        per_page: 40,
+      });
+
+      const response = await axios.get(`${BASE_URL}?${params}`);
+
+      const data = await response.data;
+
+      this.page += 1;
+      this.totalHits = response.data.totalHits;
+
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //   set nameCategory(newName) {
+  //     this.name = newName;
+  //   }
 }
